b37ef7dd80c2d00697ecdc4dced2937b649ccca1
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-21 12:00:00 +0430

    upgrade models

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Province extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProvinceFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('ولایت')
+        ->setDescriptionForEvent(fn(string $eventName) => "Province has been {$eventName}");
+    }
 }

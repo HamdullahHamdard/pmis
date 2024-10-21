e7f8f636b394ba8d36a303da83d4a8b8584fae2e
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-21 12:00:00 +0430

    upgrade models

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class SubCategory extends Model
 {
-    /** @use HasFactory<\Database\Factories\SubCategoryFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    // Sub-category relation with category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('کتگوری کوچک')
+        ->setDescriptionForEvent(fn(string $eventName) => "Sub-category has been {$eventName}");
+    }
 }

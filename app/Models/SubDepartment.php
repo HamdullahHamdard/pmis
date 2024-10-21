3bcf94544ae8871724290b7a51532780574adf1e
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-21 12:00:00 +0430

    upgrade models

--- app/Models/SubDepartment.php
+++ app/Models/SubDepartment.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class SubDepartment extends Model
 {
-    /** @use HasFactory<\Database\Factories\SubDepartmentFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    // Sub-department relation with department
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('آمریت / ریاست کوچک')
+        ->setDescriptionForEvent(fn(string $eventName) => "Sub-department has been {$eventName}");
+    }
 }

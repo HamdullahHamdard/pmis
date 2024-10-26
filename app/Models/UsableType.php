fa3371a96488107d3830a1e72a419c01b8a60c2d
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-26 12:00:00 +0430

    upgrade models

--- app/Models/UsableType.php
+++ app/Models/UsableType.php
@@ -4,9 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class UsableType extends Model
 {
-    /** @use HasFactory<\Database\Factories\UsableTypeFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    // Usable type relation with usables
+    public function usables()
+    {
+        return $this->hasMany(Usable::class);
+    }
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('نوعیت جنس مصرفی')
+        ->setDescriptionForEvent(fn(string $eventName) => "Usable item type has been {$eventName}");
+    }
 }

fe2bc850ca786cb274d99764acee270d09e19bb8
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-31 12:00:00 +0430

    upgrade migrations

--- database/migrations/2025_03_24_045339_create_employee_shifts_table.php
+++ database/migrations/2025_03_24_045339_create_employee_shifts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('employee_shifts', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
             $table->timestamps();
         });
     }

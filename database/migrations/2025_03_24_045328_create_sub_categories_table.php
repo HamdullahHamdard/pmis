e98b647a2557af1c6efe5aaa06261fa504dbd205
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-10-31 12:00:00 +0430

    upgrade migrations

--- database/migrations/2025_03_24_045328_create_sub_categories_table.php
+++ database/migrations/2025_03_24_045328_create_sub_categories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,11 +15,7 @@ return new class extends Migration
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('category_id');
-
-            $table->foreign('category_id')
-                ->references('id')
-                ->on('categories');
+            $table->foreignIdFor(Category::class);
             $table->timestamps();
         });
     }

eaa3a55afe74d0a3179f40478bf99a9f3a29b424
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-05 12:00:00 +0430

    upgrading controllers

--- app/Http/Controllers/Form9ItemController.php
+++ app/Http/Controllers/Form9ItemController.php
@@ -20,8 +20,6 @@ class Form9ItemController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -30,9 +28,6 @@ class Form9ItemController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreForm9ItemRequest  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(StoreForm9ItemRequest $request)
     {
@@ -41,9 +36,6 @@ class Form9ItemController extends Controller
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Form9Item  $form9Item
-     * @return \Illuminate\Http\Response
      */
     public function show(Form9Item $form9Item)
     {
@@ -52,9 +44,6 @@ class Form9ItemController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Form9Item  $form9Item
-     * @return \Illuminate\Http\Response
      */
     public function edit(Form9Item $form9Item)
     {
@@ -63,10 +52,6 @@ class Form9ItemController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateForm9ItemRequest  $request
-     * @param  \App\Models\Form9Item  $form9Item
-     * @return \Illuminate\Http\Response
      */
     public function update(UpdateForm9ItemRequest $request, Form9Item $form9Item)
     {
@@ -75,9 +60,6 @@ class Form9ItemController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Form9Item  $form9Item
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Form9Item $form9Item)
     {

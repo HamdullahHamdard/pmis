6bcf50764912b5c16f4537cfc572e728ce1ec7b6
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-05 12:00:00 +0430

    upgrading controllers

--- app/Http/Controllers/Form8Controller.php
+++ app/Http/Controllers/Form8Controller.php
@@ -5,27 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Form8;
 use App\Http\Requests\StoreForm8Request;
 use App\Http\Requests\UpdateForm8Request;
+use App\Models\Unit;
 
 class Form8Controller extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('form8.index');
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $units = Unit::all();
+        return view('form8.create', compact('units'));
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreForm8Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(StoreForm8Request $request)
     {
@@ -34,6 +43,9 @@ class Form8Controller extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Form8  $form8
+     * @return \Illuminate\Http\Response
      */
     public function show(Form8 $form8)
     {
@@ -42,6 +54,9 @@ class Form8Controller extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form8  $form8
+     * @return \Illuminate\Http\Response
      */
     public function edit(Form8 $form8)
     {
@@ -50,6 +65,10 @@ class Form8Controller extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateForm8Request  $request
+     * @param  \App\Models\Form8  $form8
+     * @return \Illuminate\Http\Response
      */
     public function update(UpdateForm8Request $request, Form8 $form8)
     {
@@ -58,6 +77,9 @@ class Form8Controller extends Controller
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form8  $form8
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Form8 $form8)
     {

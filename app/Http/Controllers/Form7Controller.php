544345c59a27abfd3bf78623f3d712a33a465852
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-05 12:00:00 +0430

    upgrading controllers

--- app/Http/Controllers/Form7Controller.php
+++ app/Http/Controllers/Form7Controller.php
@@ -5,27 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Form7;
 use App\Http\Requests\StoreForm7Request;
 use App\Http\Requests\UpdateForm7Request;
+use App\Models\Unit;
 
 class Form7Controller extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('form7.index');
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $units = Unit::all();
+        return view('form7.create', compact('units'));
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreForm7Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(StoreForm7Request $request)
     {
@@ -34,6 +43,9 @@ class Form7Controller extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Form7  $form7
+     * @return \Illuminate\Http\Response
      */
     public function show(Form7 $form7)
     {
@@ -42,6 +54,9 @@ class Form7Controller extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form7  $form7
+     * @return \Illuminate\Http\Response
      */
     public function edit(Form7 $form7)
     {
@@ -50,6 +65,10 @@ class Form7Controller extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateForm7Request  $request
+     * @param  \App\Models\Form7  $form7
+     * @return \Illuminate\Http\Response
      */
     public function update(UpdateForm7Request $request, Form7 $form7)
     {
@@ -58,6 +77,9 @@ class Form7Controller extends Controller
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form7  $form7
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Form7 $form7)
     {

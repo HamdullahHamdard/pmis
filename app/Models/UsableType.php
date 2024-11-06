3de8daeba90c6b92b49b209d27770feda4ba06ce
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-06 12:00:00 +0430

    upgrade models

---
+++ app/Models/UsableType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UsableType extends Model
+{
+    /** @use HasFactory<\Database\Factories\UsableTypeFactory> */
+    use HasFactory;
+}

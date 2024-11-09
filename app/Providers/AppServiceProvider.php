e980fac8408880d23c058185011e34b49b35cc58
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-09 12:00:00 +0430

    Add form8s feuture

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\Permission\Models\Permission;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        // In AppServiceProvider boot()
-Permission::firstOrCreate(['name' => 'view-stock']);
-Permission::firstOrCreate(['name' => 'view-usables']);
     }
 }

8875525cafd991aa7ded99236baa9f7b4c991111
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-16 12:00:00 +0430

    upgrade controllers requests

--- app/Http/Requests/StoreForm5Request.php
+++ app/Http/Requests/StoreForm5Request.php
@@ -8,21 +8,33 @@ class StoreForm5Request extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, mixed>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
             //
+            // "requested_dept_id"=> "required|integer",
+            // "submission_id"=> "required|integer",
+            // "distribution_no"=> "required|integer",
+            // "distribution_date"=> "required|string",
+            // "warehouse_distributer_name"=> "required|string",
+            // "form9_number"=> "required|integer",
+            // "form9_date"=> "required|string",
+            // "details"=> "required|string",
+            // "total"=> "required|integer",
+
         ];
     }
 }

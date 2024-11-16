79e6091f9acb13175280673573ab0c919179583c
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-16 12:00:00 +0430

    upgrade models

--- app/Models/Day.php
+++ app/Models/Day.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Day extends Model
 {
-    /** @use HasFactory<\Database\Factories\DayFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('روز هجری شمسی')
+        ->setDescriptionForEvent(fn(string $eventName) => "HS day has been {$eventName}");
+    }
 }

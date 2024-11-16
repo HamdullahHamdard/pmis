e8338f3d195f9ce096fe2d2288b05e721ca87534
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-16 12:00:00 +0430

    upgrade models

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Currency extends Model
 {
-    /** @use HasFactory<\Database\Factories\CurrencyFactory> */
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ["name"];
+
+    // Activity Log Options
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->useLogName('واحد پولی')
+        ->setDescriptionForEvent(fn(string $eventName) => "Currency has been {$eventName}");
+    }
 }

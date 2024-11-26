0fac5eb2d72ddd0a42954abd07fa6eb261cf21e8
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-11-26 12:00:00 +0430

    Add form8s feuture

--- resources/views/form8/index.blade.php
+++ resources/views/form8/index.blade.php
@@ -58,6 +58,12 @@
                         <tbody>
                             @foreach ($form8s as $form8)
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-md">
+                                    @if(auth()->user()->hasRole('Admin'))
+                                        <th scope="row"
+                                            class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $form8->id }}
+                                        </th>
+                                    @endif
                                     <th scope="row"
                                         class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                                         {{ $form8->id }}

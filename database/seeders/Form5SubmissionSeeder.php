2aaea5d6c9a23184d94c34eea42319f96c86a2e8
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-01-26 12:00:00 +0430

    upgrade seeders

--- database/seeders/Form5SubmissionSeeder.php
+++ database/seeders/Form5SubmissionSeeder.php
@@ -4,14 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class Form5SubmissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         //
+        DB::table("form5_submission")->delete();
+
+        DB::table("form5_submission")->insert([
+            0 => [
+                "form5_id" => 1,
+                "submission_id" => 1,
+            ],
+            1 => [
+                "form5_id" => 1,
+                "submission_id" => 2,
+            ],
+        ]);
     }
 }

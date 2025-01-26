5d7dfc355fc5a35172735395dd0c37c61d5c3e19
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-01-26 12:00:00 +0430

    upgrade seeders

--- database/seeders/Form9Seeder.php
+++ database/seeders/Form9Seeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class Form9Seeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table("form9s")->delete();
+
+        DB::table("form9s")->insert([
+            0 => [
+                'department_id' => 1,
+                'employee_id' => 1,
+                // 'requested_management'=> "مدیریت عمومی ترانسپورت",
+                'form_date' => "1403/05/22",
+                // 'first_details' => "کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند",
+                // 'second_details' => "کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند، کارمندانی که این جنس را تسلیم شده اند",
+                // 'manager_name' => "عبدالبشیر",
+
+
+            ],
+        ]);
+
+
     }
 }

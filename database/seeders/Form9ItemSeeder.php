0f8227fef5d0e05318d5080735bdf392f82d5e57
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-01-26 12:00:00 +0430

    upgrade seeders

--- database/seeders/Form9ItemSeeder.php
+++ database/seeders/Form9ItemSeeder.php
@@ -4,14 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class Form9ItemSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table("form9_item")->delete();
+
+        DB::table("form9_item")->insert([
+            0 => [
+                "form9_id" => 1,
+                "item_id" => 1,
+                "quantity" => 3,
+            ],
+            1 => [
+                "form9_id" => 1,
+                "item_id" => 2,
+                "quantity" => 2,
+            ],
+        ]);
     }
 }

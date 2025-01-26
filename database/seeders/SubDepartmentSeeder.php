0306f294dcbaa9c16a218d9f819cb50f1550fad5
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-01-26 12:00:00 +0430

    upgrade seeders

--- database/seeders/SubDepartmentSeeder.php
+++ database/seeders/SubDepartmentSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubDepartmentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table("sub_departments")->delete();
+
+        DB::table("sub_departments")->insert([
+            0 => [
+                "name" => "آمریت دفتر",
+                "department_id" => "1",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            1 => [
+                "name" => "آمریت فرستنده ها",
+                "department_id" => "2",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+        ]);
     }
 }

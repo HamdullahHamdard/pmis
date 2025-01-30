f1d31958418429895007c5357659b29c943a4390
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-01-30 12:00:00 +0430

    upgrade models

---
+++ app/Http/Controllers/Form5SubmissionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form5Submission;
+use App\Http\Requests\StoreForm5SubmissionRequest;
+use App\Http\Requests\UpdateForm5SubmissionRequest;
+
+class Form5SubmissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreForm5SubmissionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Form5Submission $form5Submission)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Form5Submission $form5Submission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateForm5SubmissionRequest $request, Form5Submission $form5Submission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Form5Submission $form5Submission)
+    {
+        //
+    }
+}

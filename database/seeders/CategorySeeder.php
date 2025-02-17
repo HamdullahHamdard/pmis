ec765f35163dabc6739995d5048ef470e822e7b1
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-02-17 12:00:00 +0430

    upgrade seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,47 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table("categories")->delete();
+
+        DB::table("categories")->insert([
+            0 => [
+                "name" => "آی ټی",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            1 => [
+                "name" => "اداری",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            2 => [
+                "name" => "تخنیکی",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            3 => [
+                "name" => "نشراتی",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            4 => [
+                "name" => "ډیجیټل میډیا",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            5 => [
+                "name" => "مصرفی",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+        ]);
     }
 }

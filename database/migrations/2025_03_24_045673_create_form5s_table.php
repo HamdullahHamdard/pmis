2c01b313aef0de24487a6f83f07c604a8cfd44a0
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-02-17 12:00:00 +0430

    upgrade migrations

--- database/migrations/2025_03_24_045673_create_form5s_table.php
+++ database/migrations/2025_03_24_045673_create_form5s_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Province;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('form5s', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("form9s_id");
+            $table->foreignIdFor(Province::class);
+            $table->string('distribution_warehouse');
+            $table->string('distribution_date');
+            $table->longText("details")->nullable();
+            $table->foreign('form9s_id')->references('id')->on('form9s')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }

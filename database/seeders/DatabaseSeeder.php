ebf69322e1e9f36d4afd6805a46ff707d1ca473a
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-02-17 12:00:00 +0430

    upgrade seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,46 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\UsableType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(ProvinceSeeder::class);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(CreateAdminUserSeeder::class);
+        $this->call(YearSeeder::class);
+        $this->call(MonthSeeder::class);
+        $this->call(DaySeeder::class);
+        $this->call(DepartmentSeeder::class);
+        $this->call(SubDepartmentSeeder::class);
+        $this->call(UnitSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(EmployeeShiftSeeder::class);
+        $this->call(EmployeeTypeSeeder::class);
+        $this->call(EmployeeSeeder::class);
+        $this->call(CurrencySeeder::class);
+        $this->call(ItemSeeder::class);
+        $this->call(SubmissionSeeder::class);
+        $this->call(UsableTypeSeeder::class);
+        $this->call(UsableSeeder::class);
+        $this->call(UsableSubmissionSeeder::class);
+        $this->call(Form9Seeder::class);
+        $this->call(UsableSubmissionSeeder::class);
+        $this->call(Form5Seeder::class);
+        $this->call(Form5SubmissionSeeder::class);
+        $this->call(Form9ItemSeeder::class);
     }
 }

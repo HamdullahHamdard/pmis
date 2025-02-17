aea3fc658ddde35ae50bd9eedae07925be9b75e1
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-02-17 12:00:00 +0430

    upgrade seeders

--- database/seeders/EmployeeShiftSeeder.php
+++ database/seeders/EmployeeShiftSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeShiftSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table("employee_shifts")->delete();
+
+        DB::table("employee_shifts")->insert([
+            0 => [
+                "name" => "ټوله ورځ",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            1 => [
+                "name" => "له سهاره تر غرمه",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            2 => [
+                "name" => "له غرمه تر ماښامه",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+            3 => [
+                "name" => "ټوله شپه",
+                "created_at" => Carbon::now()->format("Y-m-d H:i:s"),
+            ],
+        ]);
     }
 }

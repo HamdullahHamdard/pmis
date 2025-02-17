ffd3a55a0915d007627e85b7e010010b880f36e3
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-02-17 12:00:00 +0430

    upgrade migrations

--- database/migrations/2025_03_24_045708_create_form5_submissions_table.php
+++ database/migrations/2025_03_24_045708_create_form5_submissions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Form5;
+use App\Models\Submission;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('form5_submissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Form5::class);
+            $table->foreignIdFor(Submission::class);
             $table->timestamps();
         });
     }

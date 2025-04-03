274ea048d7b883af6b01170ff7d3580665210651
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-04-03 12:00:00 +0430

    Add form8s feuture

--- database/migrations/2025_03_24_045740_create_form8s_table.php
+++ database/migrations/2025_03_24_045740_create_form8s_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Form5;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('form8s', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Form5::class);
             $table->string('form8_number');
             $table->string('trusted');
             $table->unsignedBigInteger("purchaseYear_id");

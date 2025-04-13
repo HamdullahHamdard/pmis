e7afd2e7d723d4358ad065d68a896355fc92b64f
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-04-13 12:00:00 +0430

    upgrade migrations and seeders

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,19 +16,19 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    function __construct()
-    {
-        $this->middleware("permission:view-categories");
-        $this->middleware("permission:create-categories", [
-            "only" => ["create", "store"],
-        ]);
-        $this->middleware("permission:edit-categories", [
-            "only" => ["edit", "update"],
-        ]);
-        $this->middleware("permission:delete-categories", [
-            "only" => ["destroy"],
-        ]);
-    }
+    // function __construct()
+    // {
+    //     $this->middleware("permission:view-categories");
+    //     $this->middleware("permission:create-categories", [
+    //         "only" => ["create", "store"],
+    //     ]);
+    //     $this->middleware("permission:edit-categories", [
+    //         "only" => ["edit", "update"],
+    //     ]);
+    //     $this->middleware("permission:delete-categories", [
+    //         "only" => ["destroy"],
+    //     ]);
+    // }
 
     /**
      * Display a listing of the resource.

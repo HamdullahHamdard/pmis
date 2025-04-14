bd1cc4b4825e03b16df735f3ea67d48b9eb92253
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-04-14 12:00:00 +0430

    upgrade migrations and seeders

--- app/Http/Controllers/Form9Controller.php
+++ app/Http/Controllers/Form9Controller.php
@@ -18,19 +18,19 @@ class Form9Controller extends Controller
 {
 
 
-    function __construct()
-    {
-        $this->middleware("permission:view-employees");
-        $this->middleware("permission:create-employees", [
-            "only" => ["create", "store"],
-        ]);
-        $this->middleware("permission:edit-employees", [
-            "only" => ["edit", "update"],
-        ]);
-        $this->middleware("permission:delete-employees", [
-            "only" => ["destroy"],
-        ]);
-    }
+    // function __construct()
+    // {
+    //     $this->middleware("permission:view-employees");
+    //     $this->middleware("permission:create-employees", [
+    //         "only" => ["create", "store"],
+    //     ]);
+    //     $this->middleware("permission:edit-employees", [
+    //         "only" => ["edit", "update"],
+    //     ]);
+    //     $this->middleware("permission:delete-employees", [
+    //         "only" => ["destroy"],
+    //     ]);
+    // }
     /**
      * Display a listing of the resource.
      *

80ade43eb16ea3804e0568c23128fe41b3085ec9
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2025-04-20 12:00:00 +0430

    Add form8s feuture

--- app/Models/Form8Submission.php
+++ app/Models/Form8Submission.php
@@ -9,4 +9,11 @@ class Form8Submission extends Model
 {
     /** @use HasFactory<\Database\Factories\Form8SubmissionFactory> */
     use HasFactory;
+
+
+    protected $fillable = [
+        'item_id',
+        'employee_id',
+        'total'
+    ];
 }
